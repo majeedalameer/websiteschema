757c31bd4f7c32cb47b47fa56c876d60b6e1f731
Yingrui.F@gmail.com <Yingrui.F@gmail.com@6b2d01ea-ea56-11de-9229-05bbbb8527c3>
2010-10-24 15:56:27 +0000

    demo of function block

--- trunk/lib.wrapper/src/main/scala/websiteschema/wrapper/fb/DemoFB.scala
+++ trunk/lib.wrapper/src/main/scala/websiteschema/wrapper/fb/DemoFB.scala
@@ -5,6 +5,38 @@
 
 package websiteschema.wrapper.fb
 
-class DemoFB {
+import scala.actors.Actor._
+import websiteschema.wrapper.Wrapper
+import scala.collection.mutable.Map
+import scala.collection.mutable.Set
+import websiteschema.wrapper.fb._
 
+abstract class DemoFB(name:String, params:Map[String,String], wrapper:Wrapper)
+  extends FB(name:String, params:Map[String,String], wrapper:Wrapper)
+{
+
+  var date:String = "hello";
+  val relativeDataLinks = Set(EventDataLink("date","date"))
+
+  def setInputData(input:String, obj:Any):Unit = {
+    if(input.equals("date") && obj.isInstanceOf[String])
+        date = obj.asInstanceOf[String]
+  }
+  
+  def getOutputData(output:String):Any = if("date" equals output) date
+
+  def process(event:String):Unit = event match {
+    case "print" => {
+        println(date)
+        wrapper ! BasicEvent(name,"date")
+    }
+    case "end" => {
+        println("task over")
+        wrapper ! EndEvent()
+    }
+    case _ => println("invalid event")
+  }
+
+  def getRelativeDataLinksOf(event:String):Set[EventDataLink] = relativeDataLinks
+  
 }
