896a0ab200c79b1ec8c39c23bc701bdb4b6554f4
yingrui.f <Yingrui.F@gmail.com>
2010-11-14 16:12:40 +0000

    Ray - FB Design

--- lib.wrapper/src/main/scala/websiteschema/wrapper/Wrapper.scala
+++ lib.wrapper/src/main/scala/websiteschema/wrapper/Wrapper.scala
@@ -23,7 +23,7 @@ class Wrapper extends Actor {
   def act() = {
     while(!stop) {
       receive {
-        case EndEvent() => stop = true
+        case EndEvent(result) => stop = true
         case event:BasicEvent => process(event)
       }
     }
@@ -31,45 +31,57 @@ class Wrapper extends Actor {
 
   def isStoped = stop
 
-  def process(event:BasicEvent) {
-    delivery(event)
-    execute(event)
-  }
+  def process(event:BasicEvent) = 
+    if(!stop) {
+      delivery(event)
+      execute(event)
+    }
 
-  def getFB(fbName: String) = funcBlocks.get(fbName).get
-  private def getFB(event:BasicEvent):FB = getFB(event.fb)
+  def getFB(fbName: String) = funcBlocks.get(fbName)
+  private def getFB(event:BasicEvent):Option[FB] = getFB(event.fb)
 
   private def execute(event:BasicEvent){
-    val events = fbLinks.getEventLinks(event.fb, event.name)
-    events foreach invokeFB
+    val destFBs = fbLinks.getEventLinks(event.fb, event.name)
+    destFBs foreach invokeFB
 
     def invokeFB(eventLink:EventLink) {
-      val fb = getFB(eventLink.dest)
-      val event = eventLink.inputEvent
-      fb process event
-    }
-  }
-
-
-  private def delivery(event:BasicEvent) {
-    val fb = getFB(event)
-    val relativeDatas = fb getRelativeDataLinksOf event.name
-    relativeDatas.foreach(transportEventRelativeData(fb, _))
-
-    def transportEventRelativeData(fb:FB, eventDataLink:EventDataLink): Unit = {
-      val output = eventDataLink.relativeData
-      val dataLinks = fbLinks.getDataLinks(fb.getName, output)
-      dataLinks foreach transportData
-
-      def transportData(dataLink:DataLink):Unit = {
-        val src = getFB(dataLink.src)
-        val dest = getFB(dataLink.dest)
-        val delivery = src.getOutputData(dataLink.outputData)
-        dest.setInputData(dataLink.destInput, delivery)
+      getFB(eventLink.dest) match {
+        case Some(fb) =>
+          val event = eventLink.inputEvent
+          fb ! event
+        case _ => 
       }
     }
   }
 
+  private def delivery(event:BasicEvent) {
+    getFB(event) match {
+      case Some(fb) => {
+          val relativeDatas = fb getRelativeDataLinksOf event.name
+          relativeDatas.foreach(transportEventRelativeData(fb, _))
+
+          def transportEventRelativeData(fb:FB, eventDataLink:EventDataLink): Unit = {
+            val output = eventDataLink.relativeData
+            val dataLinks = fbLinks.getDataLinks(fb.getName, output)
+            dataLinks foreach transportData
+
+            def transportData(dataLink:DataLink):Unit = {
+              getFB(dataLink.src) match {
+                case Some(src) =>
+                  getFB(dataLink.dest) match {
+                    case Some(dest) =>
+                      val delivery = src.getOutputData(dataLink.outputData)
+                      dest.setInputData(dataLink.destInput, delivery)
+                    case _ =>
+                  }
+                case _ =>
+              }
+            }
+          }
+        }
+      case _ => println("getFB return None")
+    }
+  }
 
 }
 
