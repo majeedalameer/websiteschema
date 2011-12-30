e9efbd2e1dec36de6e61e9ef3fa9046c56aaf062
yingrui.f@gmail.com <yingrui.f@gmail.com@6b2d01ea-ea56-11de-9229-05bbbb8527c3>
2011-12-30 11:32:48 +0000

    clustering process finished

--- trunk/lib-cluster/src/main/java/websiteschema/cluster/analyzer/ClusterAnalyzerImpl.java
+++ trunk/lib-cluster/src/main/java/websiteschema/cluster/analyzer/ClusterAnalyzerImpl.java
@@ -4,7 +4,7 @@
  */
 package websiteschema.cluster.analyzer;
 
-import net.sf.json.JSONObject;
+import java.io.IOException;
 import java.util.ArrayList;
 import websiteschema.utils.PojoMapper;
 import java.util.HashMap;
@@ -64,6 +64,13 @@ public class ClusterAnalyzerImpl implements ClusterAnalyzer {
             ret.put("InvalidNodes", toJson(result.getInvalidNodes()));
             ret.put("TitlePrefix", toJson(result.getTitlePrefix()));
             ret.put("TitleSuffix", toJson(result.getTitleSuffix()));
+            if (null != fieldAnalyzers && !fieldAnalyzers.isEmpty()) {
+                Map<String,String> map = new HashMap<String,String>();
+                for (IFieldAnalyzer fieldAnalyzer : fieldAnalyzers) {
+                    map.put(fieldAnalyzer.getFieldName(), fieldAnalyzer.getClass().getName());
+                }
+                ret.put("FieldAnalyzers", PojoMapper.toJson(map));
+            }
         } catch (Exception ex) {
             ex.printStackTrace();
         }
@@ -122,6 +129,28 @@ public class ClusterAnalyzerImpl implements ClusterAnalyzer {
         return false;
     }
 
+    private void mergeAnalysisResult(String fieldName, Map<String, String> res, Map<String, String> old) throws IOException {
+        //如果不为空，则将结果保存到Map中
+        if (null != res && !res.isEmpty()) {
+            String ori = old.get(fieldName);
+            List<Map<String, String>> list = null;
+            if (null != ori) {
+                try {
+                    list = PojoMapper.fromJson(ori, List.class);
+                } catch (Exception ex) {
+                    //可能是旧版本，会导致反序列化失败
+                    list = new ArrayList<Map<String, String>>();
+                }
+            } else {
+                list = new ArrayList<Map<String, String>>();
+            }
+            if (!alreadyHas(list, res)) {
+                list.add(res);
+                old.put(fieldName, PojoMapper.toJson(list));
+            }
+        }
+    }
+
     private void analyzeEachCluster(Cluster cluster, FeatureStatInfo statInfo, List<Sample> samples, Map<String, String> old) {
         for (IFieldAnalyzer fieldAnalyzer : fieldAnalyzers) {
             String[] types = fieldAnalyzer.getProperClusterType();
@@ -129,25 +158,8 @@ public class ClusterAnalyzerImpl implements ClusterAnalyzer {
                 if (cluster.getType().equals(type)) {
                     try {
                         Map<String, String> res = fieldAnalyzer.analyze(cluster, statInfo, result, samples);
-                        //如果不为空，则将结果保存到Map中
-                        if (null != res && !res.isEmpty()) {
-                            String ori = old.get(fieldAnalyzer.getFieldName());
-                            List<Map<String, String>> list = null;
-                            if (null != ori) {
-                                try {
-                                    list = PojoMapper.fromJson(ori, List.class);
-                                } catch (Exception ex) {
-                                    //可能是旧版本，会导致反序列化失败
-                                    list = new ArrayList<Map<String, String>>();
-                                }
-                            } else {
-                                list = new ArrayList<Map<String, String>>();
-                            }
-                            if (!alreadyHas(list, res)) {
-                                list.add(res);
-                                old.put(fieldAnalyzer.getFieldName(), PojoMapper.toJson(list));
-                            }
-                        }
+                        //保存结果res存到old中，key是fieldAnalyzer.getFieldName()
+                        mergeAnalysisResult(fieldAnalyzer.getFieldName(), res, old);
                     } catch (Exception e) {
                         e.printStackTrace();
                     }
