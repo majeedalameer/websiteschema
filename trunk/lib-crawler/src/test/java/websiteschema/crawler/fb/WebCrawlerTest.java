df8429a62cc850fcabd14ae36336fbd97b05903a
mougaidong@gmail.com <mougaidong@gmail.com@6b2d01ea-ea56-11de-9229-05bbbb8527c3>
2012-01-18 05:44:03 +0000

    a new SimpleHttpCrawler version

--- trunk/lib-crawler/src/test/java/websiteschema/crawler/fb/WebCrawlerTest.java
+++ trunk/lib-crawler/src/test/java/websiteschema/crawler/fb/WebCrawlerTest.java
@@ -4,18 +4,20 @@
  */
 package websiteschema.crawler.fb;
 
-
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.htmlparser.NodeFilter;
+import org.htmlparser.Parser;
+import org.htmlparser.filters.TagNameFilter;
+import org.htmlparser.http.ConnectionManager;
+import org.htmlparser.util.NodeList;
+import org.htmlparser.util.ParserException;
 import org.junit.Test;
 import org.w3c.dom.Document;
 import websiteschema.fb.core.RuntimeContext;
 import websiteschema.fb.core.app.Application;
-import websiteschema.model.domain.Site;
-import websiteschema.model.domain.Websiteschema;
-import websiteschema.model.domain.factory.WebsiteschemaFactory;
-import websiteschema.persistence.hbase.SampleMapper;
 import websiteschema.persistence.hbase.WebsiteschemaMapper;
-import websiteschema.persistence.rdbms.SiteMapper;
-
 
 /**
  *
@@ -23,20 +25,39 @@ import websiteschema.persistence.rdbms.SiteMapper;
  */
 public class WebCrawlerTest {
 
+    private static String URL_STR = "http://news.163.com/12/0117/11/7NVE580G00014JB5.html";
+
     @Test
     public void test() throws Exception {
 
-        String url = "http://news.163.com/12/0117/11/7NVE580G00014JB5.html?from=index";
-        String siteId = "www_163_com_1";
+        String siteId_str = "www_163_com_1";
         Application app = new Application();
         RuntimeContext context = app.getContext();
         context.loadConfigure("fb/crawler.app");
         WebsiteschemaMapper mapper = context.getSpringBeanFactory().getBean("websiteschemaMapper", WebsiteschemaMapper.class);
         FBWebCrawler fbc = new FBWebCrawler();
-        fbc.url = url;
-        fbc.schema = mapper.get(siteId);
+        fbc.url = URL_STR;
+        fbc.schema = mapper.get(siteId_str);
+        fbc.crawlerType = "websiteschema.crawler.SimpleHttpCrawler";
         fbc.fetch();
-        Document doc  = fbc.out;
-        System.out.println(doc.toString());
+        Document doc = fbc.out;
+        System.err.println(doc.getTextContent());
+    }
+
+    //@Test
+    public void test_htmlparser() throws Exception {
+
+        Parser parser = null;
+        ConnectionManager cm = Parser.getConnectionManager();
+        parser = new Parser(cm.openConnection(URL_STR));
+        NodeFilter nodeFilter = new TagNameFilter("body");
+        NodeList nodeList = null;
+        try {
+            nodeList = parser.parse(nodeFilter);
+        } catch (ParserException ex) {
+            System.out.println(ex);
+        }
+        String body_str = nodeList.elementAt(0).toHtml();
+        System.err.println(body_str);
     }
 }
