7dbafac89a90fd1ebf580432b0426f2ff163c83f
mougaidong@gmail.com <mougaidong@gmail.com@6b2d01ea-ea56-11de-9229-05bbbb8527c3>
2012-01-18 01:17:41 +0000

    a new package named 'websiteschema.fb.common.merger'built under lib-func-blk.

---
+++ trunk/lib-func-blk/src/main/java/websiteschema/fb/common/merge/DoubleMerge.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package websiteschema.fb.common.merge;
+
+/**
+ *
+ * @author mgd
+ */
+public class DoubleMerge {
+
+}
