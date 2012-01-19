50e78072e261c6946e722491a8a1ac2b3af798a2
mougaidong <mougaidong@gmail.com>
2012-01-19 06:58:45 +0000

    CrawlerTest改名为WebCrawlerTest 将SimpleCrawlerTest移动到其对应的测试包下 添加了一个FBDOMExtractor的测试类FBDOMExtractorTest 添加了一个功能块FBContentExtractor

---
+++ lib-crawler/src/test/java/websiteschema/crawler/fb/DOMExtractorTest.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package websiteschema.crawler.fb;
+
+import org.junit.Test;
+
+/**
+ *
+ * @author mgd
+ */
+public class DOMExtractorTest {
+
+    @Test
+    public void test() {
+    }
+}
